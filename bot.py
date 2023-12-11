eb57d6208edd51a04431b7f4d8a4792151eee1a7
Aliaksei Smantsar <lesha_smancer@mail.ru>
2023-12-11 23:11:38 +0300

    Add func course rates

--- bot.py
+++ bot.py
@@ -1,4 +1,6 @@
 import asyncio
+import json
+import requests
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 from datetime import datetime
@@ -9,28 +11,44 @@ from config import TOKEN
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
+
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
-    await message.answer("Привет!")
+    await message.answer("Привет!\n\nДля получения списка комманд введите /help")
+
 
 @dp.message(Command("help"))
 async def cmd_help(message: types.Message):
-    await message.answer("""Существующие команды:/n  
-                         /start - приветственное сообщение/n
-                         /help - окно справочной информации/n
-                         /course - курсы валют на сегодня""")
+    await message.answer("Существующие команды:\n/start - приветственное сообщение\n/help - окно справочной информации\
+                         \n/course - курсы валют на сегодня")
 
 @dp.message(Command("course"))
 async def cmd_course(message: types.Message):
-    # написать функционал получения текущего курса валют
-
+    dict_course = {}
+    # Список запрашиваемых курсов валют
+    list_rates = ["RUB", "USD", "EUR"]
+    res = []
+    # Получение текущей даты
     temp_data = datetime.now()
     str_data = f"{temp_data.day}.{temp_data.month}.{temp_data.year}"
-    # await message.answer(f"курсы валют на {str_data}:\n" + "\n".join(res))
+    res.append(f"Курсы валют на {str_data}:\n")
+    # Получение текущего курса валют
+    # temp_all = requests.get("https://api.nbrb.by/exrates/currencies")
+    for el in list_rates:
+        req = f"https://api.nbrb.by/exrates/rates/{el}?parammode=2"
+        request = requests.get(req)
+        temp_dict = json.loads(request.text)
+        name = f'{temp_dict["Cur_Scale"]} {temp_dict["Cur_Name"]}'
+        dict_course[name] = temp_dict["Cur_OfficialRate"]
+    for key, val in dict_course.items():
+        res.append(f"{key} = {val} бел. руб.")
+    await message.answer("\n".join(res))
+
 
 @dp.message()
 async def echo_message(msg: types.Message):
     await bot.send_message(msg.from_user.id, msg.text)
+
 
 # Запуск процесса поллинга новых апдейтов
 async def main():
