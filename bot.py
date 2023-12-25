081f6e38cb4eaf5882ec83797c1f861cc7eca26e
Aliaksei Smantsar <lesha_smancer@mail.ru>
2023-12-26 00:16:23 +0300

    add temp weather in Minsk

--- bot.py
+++ bot.py
@@ -19,8 +19,14 @@ async def cmd_start(message: types.Message):
 
 @dp.message(Command("help"))
 async def cmd_help(message: types.Message):
-    await message.answer("Существующие команды:\n/start - приветственное сообщение\n/help - окно справочной информации\
-                         \n/course - курсы валют на сегодня")
+    res = ["Существующие команды:", 
+           "/start - приветственное сообщение", 
+           "/help - окно справочной информации", 
+           "/course - курсы валют на сегодня",
+           "/weather - текущая погода по городу Минск"
+    ]
+    await message.answer("\n".join(res))
+
 
 @dp.message(Command("course"))
 async def cmd_course(message: types.Message):
@@ -42,6 +48,32 @@ async def cmd_course(message: types.Message):
         dict_course[name] = temp_dict["Cur_OfficialRate"]
     for key, val in dict_course.items():
         res.append(f"{key} = {val} бел. руб.")
+    await message.answer("\n".join(res))
+
+
+@dp.message(Command("weather"))
+async def cmd_course(message: types.Message):
+    res = ["Ответ от сервиса OpenWeatherMap:\n"]
+    # Отправка запроса к API сервису OpenWetherMap.org
+    request = requests.get("https://api.openweathermap.org/data/2.5/weather",
+                        params={
+                            'q': 'Минск', 
+                            'type': 'like', 
+                            'units': 'metric',
+                            'appid': '1f5154c1cf4498da3cc13f8c539278ec', 
+                            'lang': 'ru'
+                        })
+    temp_dict = json.loads(request.text)
+    # Формирование словаря с данными
+    d = {
+        "Город": temp_dict["name"],
+        "Облачность": temp_dict['weather'][0]['description'],
+        "Температура": f"{temp_dict['main']['temp']} °C",
+        "Влажность": f"{temp_dict['main']['humidity']} %",
+        "Скорость ветра": f"{temp_dict['wind']['speed']} м/с",
+    }
+    for key, val in d.items():
+        res.append(f"{key}:   {val}")
     await message.answer("\n".join(res))
 
 
--- test.py
+++ test.py
@@ -1,1 +1,38 @@
+import requests
+import json
 
+
+# request = requests.get("api.openweathermap.org/data/2.5/weather?q=Minsk,blr&APPID=1f5154c1cf4498da3cc13f8c539278ec")
+request = requests.get("https://api.openweathermap.org/data/2.5/weather",
+                        params={
+                            'q': 'Минск', 
+                            'type': 'like', 
+                            'units': 'metric',
+                            'appid': '1f5154c1cf4498da3cc13f8c539278ec', 
+                            'lang': 'ru'
+                        })
+temp_dict = json.loads(request.text)
+d = {
+    "Город": temp_dict["name"],
+    "Облачность": temp_dict["weather"][0]["description"], 
+    "Температура": temp_dict["main"]["temp"],
+    "Влажность": temp_dict["main"]["humidity"],
+    "Скорость ветра": temp_dict["wind"]["speed"],
+    }
+print("Ответ от сервиса OpenWeatherMap:")
+for key, val in d.items():
+    print(f"{key}: {val}")
+
+
+# weather [{'id': 804, 'main': 'Clouds', 'description': 'пасмурно', 'icon': '04n'}]
+# base stations
+# main {'temp': 3.86, 'feels_like': -2.21, 'temp_min': 3.86, 'temp_max': 3.86, 'pressure': 990, 'humidity': 91, 'sea_level': 990, 'grnd_level': 964}
+# visibility 10000
+# wind {'speed': 11.07, 'deg': 263, 'gust': 20.69}
+# clouds {'all': 100}
+# dt 1703534398
+# sys {'type': 1, 'id': 8939, 'country': 'BY', 'sunrise': 1703485656, 'sunset': 1703512282}
+# timezone 10800
+# id 625144
+# name Минск
+# cod 200
